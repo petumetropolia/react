c85efe3a245977c0732f238269d747f63f3d5a27
petu <petteri.helttula@metropolia.fi>
2023-09-25 19:07:42 +0300

    d

--- navigators/Navigator.js
+++ navigators/Navigator.js
@@ -1,15 +1,47 @@
 import React from 'react';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import {NavigationContainer} from '@react-navigation/native';
+import {Profile} from "../views/Profile";
+import {Home} from "../views/Home";
 
 const Tab = createBottomTabNavigator();
+const Stack = createNativeStackNavigator();
+
+const TabScreen = () => {
+  return (
+    <Tab.Navigator>
+    <Tab.Screen
+      name="Home"
+      component={Home}
+      options={{
+        tabBarIcon: ({color}) => <Icon name="home" color={color} />,
+      }}
+    />
+    <Tab.Screen
+      name="Profile"
+      component={Profile}
+      options={{
+        tabBarIcon: ({color}) => <Icon name="person" color={color} />,
+      }}
+    />
+  </Tab.Navigator>
+  );
+};
+
+const StackScreen = () => {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name="Single" component={Single} />
+    </Stack.Navigator>
+  );
+};
 
 const Navigator = () => {
   return (
     <NavigationContainer>
-      {/* TODO: Make tab navigator here */}
+      <StackScreen/>
     </NavigationContainer>
   );
 };
-
+  
 export default Navigator;
